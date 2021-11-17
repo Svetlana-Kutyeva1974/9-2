daf370a923eea3caa27e92a0b3705742351d5340
Svetlana Kutyova <kutyovas@yandex.ru>
2021-11-18 00:10:34 +0300

    1

--- src/js/app.js
+++ src/js/app.js
@@ -5,4 +5,5 @@ import ArrayBufferConverter from './ArrayBufferConverter.js';
 const p1 = new ArrayBufferConverter();
 p1.load();
 p1.toString();
-console.log(p1, p1.load(), p1.toString());
+console.log(p1, p1.load());
+console.log(p1.toString());
